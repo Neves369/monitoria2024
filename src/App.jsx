a5a19f938156910eb13a26237091a65ca3dfd64b
Neves369 <douglasbrian369@hotmail.com>
2024-10-28 18:16:25 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+ /**
+ * Retorna o elemento JSX que representa o componente App
+ */
+  return (
+    <>
+      <div>
+      </div>
+    </>
+  )
+}
+
+
+
+
+export default App // Exportando o componente App como exportação padrão
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import './index.css'
+import React from 'react'
+import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+
+/**
+ * Cria um elemento root no DOM para renderizar o componente App
+ */
+const root = ReactDOM.createRoot(document.getElementById('root'));
+/**
+ * Renderiza o componente App dentro do elemento root
+ */
+root.render(
+  /**
+   * Componente React.StrictMode que habilita verificações de erros e warnings
+   * para ajudar a detectar bugs e melhorar a experiência do usuário
+   */
+  <React.StrictMode>
+     {/* Componente App que representa a aplicação principal */}
+    <App />
+  </React.StrictMode>,
+);
